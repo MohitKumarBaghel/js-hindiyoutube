46c747d575d016a647b3605137ab6a66f6957bc8
mohit kumar <53558777+MohitKumarBaghel@users.noreply.github.com>
2024-08-20 06:02:45 +0000

    datatypeConversion

---
+++ 01_basics/datatypeConversion.js
@@ -0,0 +1,29 @@
+// numberc onversion 
+
+let score = 33
+let score1 = "340"
+
+// console.log(score); //33
+// console.log(typeof score); // number
+
+// console.log(score1) // 340
+// console.log(typeof score1) //string
+
+let valueInNumber = Number(score1); // 340 convert in number
+// console.log(typeof valueInNumber) // number but output will be 340 
+
+//  "340abc" is type of string, it can be easily convert into number but when user print this it shows NaN(Not a Number)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+ // console.log(booleanIsLoggedIn) // true
+ // 1 = true
+ // 0 = false
+ // "" = false
+ // "something" = true 
+
+ let number1 = 333
+ let stringNumber =String(number1);
+ console.log(number1) // 333
+ console.log(typeof stringNumber); // string
