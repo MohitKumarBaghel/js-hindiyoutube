449a797b63ff42f4db286f72a97b2a17d372f6c9
mohit kumar <53558777+MohitKumarBaghel@users.noreply.github.com>
2024-08-20 05:14:40 +0000

    variable and datatypes

---
+++ 01_basics/Datatype.js
@@ -0,0 +1,12 @@
+"use Strict"; // this is use to remiend for machine JS code as newer version
+
+// primitive data type 
+// number
+// bigint
+// string =" "
+// boolean = true/false
+// null (standalone value)
+// undefined (value is defined yet)
+  
+console.log(typeof(undefined)); // undefined
+console.log(typeof(null));   // object
---
+++ 01_basics/variable.js
@@ -0,0 +1,6 @@
+const accountID ="2345";
+let accountEmail ="mk@gmail.com";
+var accountAddress ="iffco chowk gurugram";
+AccountBranch ="Gurugra";
+
+console.table([accountID,accountEmail,accountAddress,AccountBranch])
