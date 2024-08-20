dd3ecbf601084ff0a43a956c7f87c7e4ca440809
mohit kumar <53558777+MohitKumarBaghel@users.noreply.github.com>
2024-08-20 09:00:21 +0000

    changes

--- 01_basics/Datatype.js
+++ 01_basics/Datatype.js
@@ -8,5 +8,22 @@
 // null (standalone value)
 // undefined (value is defined yet)
   
-console.log(typeof(undefined)); // undefined
-console.log(typeof(null));   // object
+// console.log(typeof(undefined)); // undefined
+// console.log(typeof(null));   // object
+
+// Non premitive 
+// Array , object, function
+
+ const student = ["Amit","Sumit","Rohit"];
+ let myObj = {
+     Name: "mohit",
+     age:22
+  }
+//   console.log(typeof student);
+//   console.log(typeof myObj);
+//   console.log(myObj);
+ 
+  const myFunction = function(){
+    console.log("Hi i am mohit"); 
+  }
+   myFunction();
