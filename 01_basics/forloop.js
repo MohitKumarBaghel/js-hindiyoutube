9993fe38f3eba52a1442b8584fc667c596e7c7a2
mohit kumar <53558777+MohitKumarBaghel@users.noreply.github.com>
2024-08-26 10:32:56 +0000

    for loop

---
+++ 01_basics/forloop.js
@@ -0,0 +1,41 @@
+ // for 
+ 
+//  for (let index = 0 ;index<10 ;index++){
+    
+//        let element = index
+//        console.log(element)
+
+ /// for loop basics
+
+    //  for (let i=1;i<=5;i++){
+    //     for(let j=1;j<=10;j++){
+    //         console.log(i + " * " + j + " = " + i*j)
+    //     }
+    //  }
+
+     // loop for array
+
+    //  let arr1 = ["Ram","Krishna","Hanuman"]
+    //  for(let i=0; i<arr1.length ; i++){
+        
+    //       let arrresult = arr1[i]
+    //       console.log(arrresult);
+    //  }
+
+    //  break and continue 
+
+    // for(let i = 0; i <= 10; i++){
+    //   if(i==5){
+    //   break;
+    //   }
+    //   console.log(`value of i is ${i}`);
+    // }
+
+    // continue 
+    for(let i = 0; i <= 10; i++){
+      if(i==5){
+      continue ;
+      }
+      console.log(`value of i is ${i}`);
+    }
+     
