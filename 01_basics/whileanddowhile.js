02eb7beff28eea323ca01887fb8392118c6639f9
mohit kumar <53558777+MohitKumarBaghel@users.noreply.github.com>
2024-08-26 12:05:20 +0000

    while and do while loop

---
+++ 01_basics/whileanddowhile.js
@@ -0,0 +1,33 @@
+// while loop 
+// let index = 1;
+// while(index <=10){
+//     console.log(`value of index is ${index}`)
+//     index++
+// }
+
+// while loop on array 
+
+// let arr1 =["Ram","krishna","Hanuman"]
+// let index =0;
+// while(index < arr1.length){
+//     console.log(arr1[index])
+//     index++;
+// }
+
+// do while loop 
+
+// let score = 0;
+//   do {    
+//     console.log(`your score is ${score}`);
+//      score++;
+//   }while(score<10);
+
+
+///  do while loop 
+
+let element = 11;
+ do{
+   
+    console.log(`your element is ${element}`)
+
+ }while(element<10);
