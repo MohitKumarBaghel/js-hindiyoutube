01a67edab946086ab5d0d12907bbd6daa809031c
mohit kumar <53558777+MohitKumarBaghel@users.noreply.github.com>
2024-08-26 09:08:00 +0000

    switch

---
+++ 01_basics/switch.js
@@ -0,0 +1,38 @@
+// const month = 3
+
+// switch(month){       
+//     case 1:  console.log("january");
+//              break;
+
+//     case 2:  console.log("februray");
+//              break;
+
+//     case 3:  console.log("march");
+//              break;
+
+//     case 4:  console.log("april");
+//              break;
+
+//      default:
+//         console.log("default case")
+    
+// }
+
+
+    /// Nullish calescing Operator (??): null undefined
+
+   let val1 = 5 ?? 10
+   console.log(val1)
+
+   let val2 = null ?? 10
+   console.log(val2)
+
+   let val3 = null ?? undefined
+   console.log(val3)
+
+   // ternarry opertaor 
+
+   const iceteaprice = 80;
+   iceteaprice >=100 ? console.log("value is greater than or equal to 80") : console.log("value is less than 80 ")
+
+   // value is less than 80
