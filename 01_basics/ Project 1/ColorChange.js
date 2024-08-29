2c313e6e3fd9b861ee55f0fc4757860f55d2bfc5
mohit kumar <53558777+MohitKumarBaghel@users.noreply.github.com>
2024-08-29 09:32:45 +0000

    changes

--- 01_basics/ Project 1/ColorChange.js
+++ 01_basics/ Project 1/ColorChange.js
@@ -14,10 +14,10 @@
             if(e.target.id === 'red'){
                 body.style.backgroundColor = e.target.id;
             }
-            if(e.target.id === 'blue'){
+            if(e.target.id === 'green'){
                 body.style.backgroundColor = e.target.id;
             }
-            if(e.target.id === 'yellow'){
+            if(e.target.id === 'purple'){
                 body.style.backgroundColor = e.target.id;
             }
                 
